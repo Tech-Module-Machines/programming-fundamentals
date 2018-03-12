f172869420b6c5561aa0161dbe4d87010a5082da
Georgi Peev <itsgosho2@gmail.com>
2018-03-12 21:03:26 +0200

    .
    added the comments to the code

--- intro-and-basic-syntax/exersices/Problem3.cs
+++ intro-and-basic-syntax/exersices/Problem3.cs
@@ -3,7 +3,9 @@ class Program
 {
     static void Main()
     {
+		//we read the miles and parse them 
         double mile = double.Parse(Console.ReadLine());
+		//and then print the to the console
         Console.WriteLine("{0:f2}", mile * 1.60934);
     }
 }
--- intro-and-basic-syntax/exersices/Problem5.cs
+++ intro-and-basic-syntax/exersices/Problem5.cs
@@ -3,12 +3,14 @@ class Program
 {
     static void Main()
     {
+		//Here we read the inputs that comes from the console
+		//and parse them
         string name = Console.ReadLine();
         int current_health = int.Parse(Console.ReadLine());
         int maximum_health = int.Parse(Console.ReadLine());
         int current_energy = int.Parse(Console.ReadLine());
         int maxium_energy = int.Parse(Console.ReadLine());
-
+         //and last we print them to the console 
         Console.WriteLine("Name: {0}", name);
         Console.WriteLine("Health: |{0}{1}|", new string('|', current_health), new string('.', maximum_health - current_health));
         Console.WriteLine("Energy: |{0}{1}|", new string('|', current_energy), new string('.', maxium_energy - current_energy));
