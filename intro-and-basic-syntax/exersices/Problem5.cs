3d1538e242a5290bca0aa2561b6baf231b7d2f8a
Tsvetalin Tsonev <tsvetalint@gmail.com>
2018-03-13 04:34:42 +0200

    add some comments

--- intro-and-basic-syntax/exersices/Problem5.cs
+++ intro-and-basic-syntax/exersices/Problem5.cs
@@ -10,7 +10,7 @@ class Program
         int maximum_health = int.Parse(Console.ReadLine());
         int current_energy = int.Parse(Console.ReadLine());
         int maxium_energy = int.Parse(Console.ReadLine());
-         //and last we print them to the console 
+         //and last we print name, health and energy to the console 
         Console.WriteLine("Name: {0}", name);
         Console.WriteLine("Health: |{0}{1}|", new string('|', current_health), new string('.', maximum_health - current_health));
         Console.WriteLine("Energy: |{0}{1}|", new string('|', current_energy), new string('.', maxium_energy - current_energy));
