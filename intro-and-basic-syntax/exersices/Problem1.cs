ac859805c0eeaf96c317184cf3b4c76c94335246
Milcho Arnaudov <37160354+milchoarnaudov@users.noreply.github.com>
2018-03-13 21:34:34 +0200

    Changed variables names.

--- intro-and-basic-syntax/exersices/Problem1.cs
+++ intro-and-basic-syntax/exersices/Problem1.cs
@@ -7,13 +7,13 @@ namespace DebitCardNumber
         static void Main(string[] args)
         {
             //We receive 4 integers on the console
-            int a = int.Parse(Console.ReadLine());
+            int firstInt = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
             int c = int.Parse(Console.ReadLine());
             int d = int.Parse(Console.ReadLine());
 
             //We print them in 4-digit debit card format
-            Console.WriteLine("{0:D4} {1:D4} {2:D4} {3:D4}", a,b,c,d);
+            Console.WriteLine("{0:D4} {1:D4} {2:D4} {3:D4}", firstInt,b,c,d);
 
         }
     }
--- intro-and-basic-syntax/exersices/Problem2.cs
+++ intro-and-basic-syntax/exersices/Problem2.cs
@@ -4,10 +4,10 @@ class Program
     static void Main()
     {
         //We recieve width and height floating point numbers on the console
-        double width = double.Parse(Console.ReadLine());
+        double widthChanged = double.Parse(Console.ReadLine());
         double height = double.Parse(Console.ReadLine());
 
         //We calculate the rectangle’s area, based on its width and height and we print it
-        Console.WriteLine("{0:f2}",width*height);
+        Console.WriteLine("{0:f2}",widthChanged*height);
     }
 }
