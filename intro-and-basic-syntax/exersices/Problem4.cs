967ed8df37543cc485cfc36b410abcecbb39984f
Georgi Peev <itsgosho2@gmail.com>
2018-03-13 14:14:08 +0200

    .
    documented the 4th solution

--- intro-and-basic-syntax/exersices/Problem4.cs
+++ intro-and-basic-syntax/exersices/Problem4.cs
@@ -3,19 +3,21 @@ import java.util.Scanner;
 
 public class P04_BeverageLabels {
     public static void main(String[] args) {
+		//We initialize the scanner to read from the console
         Scanner scan = new Scanner(System.in);
+		//the next 4 rows are what we will read
         String product = scan.nextLine();
         int volume = Integer.parseInt(scan.nextLine());
         int energyPer100ml = Integer.parseInt(scan.nextLine());
         int sugarPer100ml = Integer.parseInt(scan.nextLine());
-    
+        //we put the input in big decimal object
         BigDecimal totalEnergy = new BigDecimal(String.valueOf(energyPer100ml / 100.0 * volume));
         BigDecimal totalSugar = new BigDecimal(String.valueOf(sugarPer100ml * volume / 100.0));
-    
+     // we print the results with printf where we implemented our custom method
         System.out.printf("%dml %s:%n", volume, product);
         System.out.printf("%skcal, %sg sugars", customRound(totalEnergy), customRound(totalSugar));
     }
-
+      //Here is the our method for custom round
     static String customRound(BigDecimal number){
         String numberString = number.setScale(12,BigDecimal.ROUND_HALF_UP).toString();
         int index = 15 - numberString.indexOf('.');
